e9ae1aceca783fc3099937c9c6d3efd212179166
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-31 18:26:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,17 @@ require 'pry'
 
 def reverse_each_word(sentence1)
   
+<<<<<<< HEAD
   sentence1.split(" ")collect {|word| word.reverse}.join(" ")
+=======
+  array = sentence1.split(" ")
+  
+  array2 = array.collect do |word|
+  
+  word.reverse 
+    
+    #puts #{array2.split}   
+  end
+  array2.join(" ")
+>>>>>>> 99ff2de2bf04b58699e1c655b3bc2479db90b4dd
 end
